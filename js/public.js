13a5d3fdb785f0763077c4b066afd43800e8d6a6
zhangruyi <zhang_15755051397@163.com>
2018-05-29 17:53:13 +0800

    components

--- js/public.js
+++ js/public.js
@@ -1,6 +1,21 @@
 (function() {
     console.log(12345678);
-    alert(3333345678);
+    // alert(3333345678);
+    // new Vue()
+    // import Vue from 'vue';
+    // import App from './index.vue';
+
+    // $(function() {
+
+    //     // $('#workflowLabels').html('<div id="#apps"></div>');
+    //     window.vueinstace = new Vue({
+    //         el: '#wrap-vue',
+    //         // render: h => h(App)
+    //         render: function(h) {
+    //             return h(App);
+    //         }
+    //     });
+    // });
 })();
 module.exports = function() {
     var greet = document.createElement('div');
@@ -8,4 +23,6 @@ module.exports = function() {
     return greet;
     
 }
+
+
 
