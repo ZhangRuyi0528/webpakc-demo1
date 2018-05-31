219fd1120b67bf68089a13ad8d326a378ed1f48d
Mr.Zhang_ruyi <zhang_15755051397@163.com>
2018-05-31 23:55:52 +0800

    add webpack file

--- app.js
+++ app.js
@@ -1,41 +1,13 @@
 /**
  * app入口
- * 
  */
-
- /*
-const app = require('http'),
-    express = require('express'),
-    dir = require('fs');
-
-app.createServer((request, response) => (
-    response.writeHead(200, {'Content-Type': 'text/plain'}),
-    // 发送响应数据 "Hello World"
-    response.end('ceshi shuju'))
-).listen(5656)
-*/
-
-/****阻塞代码***/
-// let file = dir.readFileSync('./html/abc.text');
-// console.log(file.toString())
-
-/*****非阻塞代码
- * 读取public代码
- * 
-*/
-
-//     dir.readFile('./js/public.js', function(err, data) {
-//         if (err) return console.error(err);
-//         console.log(data.toString());
-//     })
-// console.log(123456789);
-
-
-
+ 
 
 /**
  * require,webpack
- * 
  * **/ 
+require('./css/index.css');
 const webp = require('./js/public.js');
-document.getElementsByTagName('body').appendChild(webp());
+
+// require("!style-loader!css-loader!./css/index.css");
+// document.getElementsByTagName('body').appendChild(webp());
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
+const webpack = require('webpack');
+
 module.exports = {
     entry:  __dirname + "/app.js",//入口文件
     output: {
       path: __dirname + "/bundle",//打包后的文件存放的地方
       filename: "entry.js"//打包后输出文件的文件名
-    }
+    },
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader" }
+        ]
+    },
+    plugins: [
+      new webpack.BannerPlugin('测试webpack 实例')
+    ]
   }
