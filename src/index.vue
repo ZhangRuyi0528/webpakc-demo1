0728a677c360dfaefc160e0061657a18705856fc
zhangruyi <zhang_15755051397@163.com>
2018-09-07 16:38:43 +0800

    feature(ico) frame

--- src/index.vue
+++ src/index.vue
@@ -1,9 +1,14 @@
 <template>
     <div>
-        <h3>vue模块</h3>
-        <ul>
-            <li v-for="item in items"><router-link :to="item.link">{{item.id}}</router-link></li>
-        </ul>
+        <h3><a href="index.html">pao</a></h3>
+        <nav id="proj_nav">
+            <ul>
+                <li v-for="item in items"><router-link :to="item.link">{{item.id}}</router-link></li>
+            </ul>
+        </nav>
+        <div id="banner">
+            <img src="" alt="">
+        </div>
         <div id="wrap-router">
             <router-view>
 
@@ -35,6 +40,46 @@
     }
 </script>
 
-<style>
+<style lang="scss">
+    /*色值设置*/
+    $color-a: #4680ff;
 
+    /*引入*/
+    @mixin border-1 ($color) {
+        border: 1px solid $color;
+    }
+    @mixin floatL {
+        float: left;
+    }
+    @mixin floatR {
+        float: left;
+    }
+
+    #proj_nav {
+        width: 61.8%;
+        height: auto;
+        margin: 0 auto;
+
+        ul {
+            display: flex;
+            border-right: 0;
+            
+            @include border-1($color: $color-a);
+
+            &>li {
+                padding: 10px 0;
+                flex-grow: 1;
+                text-align: center;
+                @include floatL;
+                @include border-1($color: $color-a);
+            }
+        }
+    }
+
+    #banner {
+        width: 80%;
+        height: 200px;
+        margin: 0 auto;
+        display: block;
+    }
 </style>
--- src/public.js
+++ src/public.js
@@ -7,8 +7,8 @@
     import 'element-ui/lib/theme-chalk/index.css';
     Vue.use(ElementUI);
 
-    import VueResource from 'vue-resource'
-    Vue.use(VueResource)
+    // import VueResource from 'vue-resource'
+    // Vue.use(VueResource)
 
     const webApp = (val) => {
         return val += 1
@@ -17,18 +17,18 @@
     console.log(12345678);
     // alert(3333345678);
 
-    $(function() {
 
         // $('#workflowLabels').html('<div id="#apps"></div>');
-        window.vueinstace = new Vue({
-            el: '#wrap-vue',
+    window.onload = function() {
+        new Vue({
+            el: document.getElementById('wrap-vue'),
             router,
             // render: h => h(App)
             render: function(h) {
                 return h(App);
             }
         });
-    });
+    }
 // })();
 // module.exports = function() {
 //     var greet = document.createElement('div');
